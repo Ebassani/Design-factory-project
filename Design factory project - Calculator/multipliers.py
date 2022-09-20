06bad044a5570f624da9072d0439be7c2ba67f3e
slavakich7 <slavakich7@gmail.com>
2022-09-20 09:19:49 +0300

    transport.py

--- Design factory project - Calculator/multipliers.py
+++ Design factory project - Calculator/multipliers.py
@@ -2,12 +2,13 @@
 #Common variables, used in multiple classes
 AverageElectricity = 281.00
 
-class flights:
+def flights():
     #kgCO2e/hkm
     shortFlightUnder463km = 0.2595*2+0.2595*2
     longFligtsOverHomelandOver463km = (0.178*2)+(0.178*0.2)
     longFlightsAbroadOver463km = (0,149*2)+(0.149*0.2)
     longFlightsOver3700km = (0.135*2)+(0.135*0.2)
+    return
     
 class carTraffic:
     #kgCO2e/km
