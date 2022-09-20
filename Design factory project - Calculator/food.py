c3262c65df7fb1e806d4e782d0903903c425768b
Pheb63 <89520387+Pheb63@users.noreply.github.com>
2022-09-20 09:16:16 +0300

    defs and returns

--- Design factory project - Calculator/food.py
+++ Design factory project - Calculator/food.py
@@ -5,34 +5,36 @@ amountOfPortions = input("How many portions we're made")
 
 def meatmeal():
 
-    Co2OfMeatMeal = amountOfPortions * (meatMealProtein + meatMealSide + Egg + Dairy)
+    
     meatMealProtein = input("Which protein does the meal have? Fish, Pig, Cow, Chicken or Sausage?")
     meatMealSide = input("Choose the side of the meal. Rice, pasta, potato or vegetables")
     Egg = input("Does the meal have eggs?")
     Dairy = input ("Does the meal have dairy products?")
+    Co2OfMeatMeal = amountOfPortions * (meatMealProtein + meatMealSide + Egg + Dairy)
+    return Co2OfMeatMeal
 
 def veggiemeal():
     veggiemealProtein = input("Which protein does the meal have? Soybean, Tofu, vegetables or horse bean?")
     veggiemealSide = input("Choose the side of the meal. Rice, pasta, potato or vegetables")
     Egg = input("Does the meal have eggs?")
     Dairy = input ("Does the meal have dairy products?")
-    Co2OfVeggieMeal = amountOfPortions * (veggiemealProtein + veggiemealSide + Egg + Dairy)
+    return (amountOfPortions * (veggiemealProtein + veggiemealSide + Egg + Dairy))
 
 def veganmeal():
     veganmealProtein = input("Which protein does the meal have? Soybean, Tofu, vegetables or horse bean?")
     veganmealSide = input("Choose the side of the meal. Rice, pasta, potato or vegetables")
-    Co2OfVeganMeal = amountOfPortions * (veganmealProtein + veganmealSide)
+    return (amountOfPortions * (veganmealProtein + veganmealSide))
 
 def dessert():
     pancake = amountOfPortions * 0.206
     kisel = amountOfPortions * 0.040
-    Co2OfDessert = amountOfPortions * (pancake + kisel)
+    return (amountOfPortions * (pancake + kisel))
 
 def others():
     ryeBread = (amountOfPortions * 1.3/1000*30)
     wheatBread = (amountOfPortions * 1.7/1000*30)
     salad = amountOfPortions * 0.265
-    Co2OfOthers = amountOfPortions * (ryeBread + wheatBread + salad)
+    return (amountOfPortions * (ryeBread + wheatBread + salad))
 
 #The next couple classes use complicated formulas in excel which I have no motivation to do right now because they look like hell on earth :DDDDDD
 #Includes surplusfood foodDistribution and electricity
@@ -43,9 +45,10 @@ def surplusFood():
     veggie = amountKG #=(=VLOOKUP(E13,Kertoimet!B133:C136,2,FALSE)/1000*170)/170)*amountKG*1000
     vegan = amountKG #=(=VLOOKUP(E19,Kertoimet!B133:C136,2,FALSE)/1000*170)/170)*amountKG*1000
     sides = amountKG #=((=VLOOKUP(E8,Kertoimet!B145:C148,2,FALSE)/1000*150)/150+(=VLOOKUP(E14,Kertoimet!B145:C148,2,FALSE)/1000*150)/150+(=VLOOKUP(E20,Kertoimet!B145:C148,2,FALSE)/1000*150)/150)/3*amountKG*1000
-    salad = multipliers.salad / 150 * 1000 * amountKG
+    salad = multipliers.aPortionOfSalad / 150 * 1000 * amountKG
     bread = (multipliers.ryeBread + multipliers.wheatBread) / 2 * amountKG
-    dessert = ((multipliers.kisel + multipliers.pancakes)/2)/150*1000*amountKG
+    dessert = ((multipliers.PortionOfKisel + multipliers.aPortionOfPancakes)/2)/150*1000*amountKG
+    return meat + veggie + vegan + sides + salad + bread + dessert
 
 def foodDistribution():
     costOfaMeal =  input("Cost of the meal in €")
@@ -53,17 +56,19 @@ def foodDistribution():
     meat = "temp"#=IF(I19<>"",I19/J17*(F7+F8+F9+F10+Kertoimet!C178+1.5/1000*30),J19*(F7+F8+F9+F10+Kertoimet!C178+1.5/1000*30))
     veggie = "temp"#=IF(I22<>"",I22/J17*(F13+F14+F15+F16+Kertoimet!C178+1.5/1000*30),J22*(F13+F14+F15+F16+Kertoimet!C178+1.5/1000*30))
     vegan = "formula"#=IF(I25<>"",I25/J17*(F19+F20+Kertoimet!C178+1.5/1000*30),J25*(F19+F20+Kertoimet!C178+1.5/1000*30))
+    return costOfaMeal + amountOfMeals + meat + veggie + vegan
 
 def electricity():
     cookingAndCentralKitchen = foodDistribution.amountOfMeals or foodDistribution.costOfaMeal + "formula" #=IF(Kertoimet!D58<>"",Kertoimet!C58*Kertoimet!C82*I30,Kertoimet!C56*Kertoimet!C82*I30)
     cookingAndHeatingKitchen =  foodDistribution.costOfaMeal or foodDistribution.amountOfMeals + "formula" #=IF(Kertoimet!D58<>"",Kertoimet!C58*Kertoimet!C83*I31,Kertoimet!C56*Kertoimet!C83*I31)
+    return cookingAndCentralKitchen + cookingAndHeatingKitchen
 
 def overview():
     cafeteriaConsumption = electricity.cookingAndCentralKitchen + electricity.cookingAndHeatingKitchen
     surplusFoodCO2Footprint = surplusFood.meat + surplusFood.veggie + surplusFood.vegan + surplusFood.sides + surplusFood.salad + surplusFood.bread + surplusFood.dessert
     foodDistributionCO2Footprint = foodDistribution.meat + foodDistribution.veggie + foodDistribution.vegan
     foodCO2Footprint = meatmeal.Co2OfMeatMeal + veggiemeal.Co2OfVeggieMeal + veganmeal.Co2OfVeganMeal + dessert.Co2OfDessert + others.Co2OfOthers
-
+    return cafeteriaConsumption + surplusFoodCO2Footprint + foodDistributionCO2Footprint + foodCO2Footprint 
     
 #Food info - Personal
 
@@ -74,7 +79,7 @@ def drinks():
     oatMilk = amountOfPortions * ((multipliers.oatDrink/1000)*170)
     coffee = amountOfPortions * ((multipliers.coffee/1000)*170)
     tea = amountOfPortions * ((multipliers.tea/1000)*170)
-    drinkTotal = water + milk + oatMilk + coffee + tea 
+    return water + milk + oatMilk + coffee + tea 
 
 def others():
     ryeBread = amountOfPortions * ((multipliers.ryeBread/1000)*30)
@@ -109,19 +114,19 @@ def proteins():
     vegetables = amountOfPortions * (multipliers.vegetables/1000*170)
     Egg = amountOfPortions * (multipliers.egg/1000*60)
     cream = amountOfPortions * (multipliers.cream/1000*60)
-    proteinsTotal = fish + cow + pig + chicken + sausage + soy + broadBean + tofu + vegetables + Egg + cream
+    return fish + cow + pig + chicken + sausage + soy + broadBean + tofu + vegetables + Egg + cream
 
 def carboHydrates():
     potato = amountOfPortions * (multipliers.potato/1000*150)
     rice = amountOfPortions * (multipliers.rice/1000*100)
     pasta = amountOfPortions * (multipliers.pasta/1000*120)
     rootVegetables = amountOfPortions * (multipliers.rootVegetables/1000*150)
-    totalCarboHydrates = potato + rice + pasta + rootVegetables
+    return potato + rice + pasta + rootVegetables
 
 def dessert(): 
     pancakes = amountOfPortions * multipliers.pancakes
     kisel = amountOfPortions * multipliers.kisel
-    totalDessert = pancakes + kisel
+    return pancakes + kisel
 
-personalLunch = amountOfPortions * (proteinsTotal + totalCarboHydrates + totalDessert)
-total = (personalLunch + drinkTotal + othersTotal + foodWaste)
+personalLunch = amountOfPortions * (proteins + carboHydrates + dessert)
+total = (personalLunch + drinks + others + foodWaste)
