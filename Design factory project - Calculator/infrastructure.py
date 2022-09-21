057100b976ee2c472dd9770d61857a0e14454caf
m4ju87 <89781297+m4ju87@users.noreply.github.com>
2022-09-22 01:17:11 +0300

    update
    save

--- Design factory project - Calculator/infrastructure.py
+++ Design factory project - Calculator/infrastructure.py
@@ -1,3 +1,4 @@
+from operator import mul
 import multipliers
 
 class userInputs:
@@ -8,9 +9,9 @@ class userInputs:
     ZeroEmissionGreenElectricityUserInput=float(input("Percantage"))
     #heating(Megawatts)
 
-    ElectricHeatinginput=float(input("MegaWattsHour"))
-    DistrictHeatinginput=float(input("MegaWattsHour"))
-    PercentageThatIsRenewableInput=float(input("Percantage"))
+    ElectricityHeatingInput=float(input("MegaWattsHour"))
+    DistrictHeatingInput=float(input("MegaWattsHour"))
+    ZeroEmissionDistrictHeatingInput=float(input("Percantage"))
 
     #Cooling(Megawatts)
     
@@ -19,58 +20,105 @@ class userInputs:
 
     #Airconditioning(Time)
 
-    AirconditionPerDay=float(input("What type of Airconditioning?removal or removal and insertional both"))
-    HoursPerDay=float(input("Hours"))
+    #AirconditionPerDay=float(input("What type of Airconditioning?removal or removal and insertional both"))
+    #HoursPerDay=float(input("Hours"))
 
     #WasteDistribution(Wieght)
 
     MixedWasteInput=float(input("Kilograms"))
     EnergyWasteInput=float(input("Kilograms"))
-    BioWasterInput=float(input("Kilograms"))
-    CardboardWaste=float(input("Kilograms"))
-    GlassWaste=float(input("Kilograms"))
-    MetalWaste=float(input("Kilograms"))
-    PlasticWaste=float(input("Kilograms"))
-    PaperWaste=float(input("Kilograms"))
-    ElectricAppliancesWaste=float(input("Kilograms"))
-    HazardousWaste=float(input("Kilograms"))
+    BioWasteInput=float(input("Kilograms"))
+    CardboardInput=float(input("Kilograms"))
+    GlassInput=float(input("Kilograms"))
+    MetalInput=float(input("Kilograms"))
+    PlasticInput=float(input("Kilograms"))
+    PaperInput=float(input("Kilograms"))
+    ElectricityRecycableInput=float(input("Kilograms"))
+    HazardousInput=float(input("Kilograms"))
 
-    #ItemsWaste(Amount)
+    #ItemsWaste/trash(Amount)
     #ITRelatedItems
 
-    MobilephoneWaste=float(input("Pieces"))
-    LaptopWaste=float(input("Pieces"))
-    ComputerMonitorWaste=float(input("Pieces"))
-    TabletWaste=float(input("Pieces"))
-    DesktopComputerWaste=float(input("Pieces"))
-    MultifunctionPrinterWaste=float(input("Pieces"))
-    PrinterWaste=float(input("Pieces"))
+    phoneInput=float(input("Pieces"))
+    LaptopInput=float(input("Pieces"))
+    computerScreenInput=float(input("Pieces"))
+    TabletInput=float(input("Pieces"))
+    DesktopComputerInput=float(input("Pieces"))
+    MultifunctionPrinterInput=float(input("Pieces"))
+    PrinterInput=float(input("Pieces"))
 
     #furtniture(Amount)
 
-    OfficeChairWaste=float(input("Pieces"))
-    ChairWaste=float(input("Pieces"))
-    TableWaste=float(input("Pieces"))
-    ElectrictableWaste=float(input("Pieces"))
+    OfficeChairInput=float(input("Pieces"))
+    ChairInput=float(input("Pieces"))
+    metalFrameTableInput=float(input("Pieces"))
+    electricDeskInput=float(input("Pieces"))
+    
 
     #Material(Amount)
 
-    PaperItemWasteWaste=float(input("Pieces"))
-    EbookWaste=float(input("Pieces"))
-    BookWaste=float(input("Pieces"))
+    paperKgInput=float(input("Kilograms"))
+    EbookInput=float(input("Pieces"))
+    BookInput=float(input("Pieces"))
 
     #OtherItems(Euros)
 
-    ClearoutAmount=float(input("Euros"))
-    InternetPhoneAmount=float(input("Euros"))
-    PostiAmount=float(input("Euros"))
+    CleaningInput=float(input("Euros"))
+    internetAndPhonesInput=float(input("Euros"))
+    postalInput=float(input("Euros"))
 
 class userEmissionEnergy(multipliers.energy):
     
-    #kgCO2e
-    ElectricityconsumptionEmission = userInputs.ElectricityconsumptionMWhUserInput*multipliers.energy.AverageElectricity
+    #Electricity consumption
+    #kgCO2e/MWh
+    ElectricityconsumptionEmission = userInputs.ElectricityconsumptionMWhUserInput*multipliers.energy.verageElectricity
     ZeroEmissionGreenElectricityEmission = userInputs.ZeroEmissionGreenElectricityUserInput*multipliers.energy.ZeroEmissionGreenElectricity
 
-    
+    ElectricityHeatingEmission = userInputs.ElectricityHeatingInput*multipliers.Heating.DistrictHeating
+    DistrictHeatingEmission = userInputs.DistrictHeatingInput*multipliers.Heating.ElectricityHeating
+    ZeroEmissionDistrictHeatingEmission = userInputs.ZeroEmissionDistrictHeatinginput*multipliers.Heating.ZeroEmissionDistrictHeating
 
+    DistrictCoolingEmission = userInputs.DistrictCoolinginput*multipliers.Cooling.DistrictCooling
+    ElectricCoolingEmission = userInputs.ElectricCoolingInput*multipliers.Cooling.ElectricityCooling
+
+    #AirconditionPerDay
+    #HoursPerDay
+
+    #Trash / kgCO2e/kg
+    MixedWasteEmission = userInputs.MixedWasteInput*multipliers.Trash.MixedWaste
+    EnergyWasteEmission = userInputs.EnergyWasteInput*multipliers.Trash.EnergyWaste
+    BioWasteEmission = userInputs.BioWasteInput*multipliers.Trash.BioWaste
+    CardboardEmission = userInputs.CardboardInput*multipliers.Trash.Cardboard
+    GlassEmission = userInputs.GlassInput*multipliers.Trash.Glass
+    MetalEmission = userInputs.MetalInput*multipliers.Trash.Metal
+    PlasticEmission = userInputs.PlasticInput*multipliers.Trash.Plastic
+    PaperEmission = userInputs.PaperInput*multipliers.Trash.Paper
+    ElectricityRecycableEmission = userInputs.ElectricityRecycableInput*multipliers.Trash.ElectricityRecycable
+    HazardousEmission = userInputs.HazardousInput*multipliers.Trash.Hazardous
+
+    #class acquisitions:
+    #kgCO2e/kpl
+
+    phoneEmission = userInputs.phoneInput*multipliers.acquisitions.phone
+    LaptopEmission = userInputs.LaptopInput*multipliers.acquisitions.Laptop
+    TabletEmission = userInputs.TabletInput*multipliers.acquisitions.Tablet
+    DesktopComputerEmission = userInputs.DesktopComputerInput*multipliers.acquisitions.DesktopComputer
+    MultifunctionPrinterEmission = userInputs.MultifunctionPrinterInput*multipliers.acquisitions.MultifunctionPrinter
+    PrinterEmission = userInputs.PrinterInput*multipliers.acquisitions.Printer
+
+
+    OfficeChairEmission = userInputs.OfficeChairInput*multipliers.acquisitions.OfficeChair
+    ChairEmission = userInputs.ChairInput*multipliers.acquisitions.Chair
+    metalFrameTableEmission = userInputs.metalFrameTableInput*multipliers.acquisitions.metalFrameTable
+    electricDeskEmission = userInputs.electricDeskInput*multipliers.acquisitions.electricDesk
+
+    paperKgEmission = userInputs.paperKgInput*multipliers.acquisitions.paperKg
     
+    #item/kgCO2e
+    EbookEmission = userInputs.EbookInput*multipliers.item.Ebook
+    BookEmission = userInputs.BookInput*multipliers.item.Book
+
+
+    CleaningEmission = userInputs.CleaningInput*multipliers.item.Cleaning
+    internetAndPhonesEmission = userInputs.internetAndPhonesInput*multipliers.item.internetAndPhones
+    postalEmission = userInputs.postalInput*multipliers.item.postal
