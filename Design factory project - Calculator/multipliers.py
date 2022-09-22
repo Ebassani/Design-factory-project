3ffe20ca486c32ee5ee090b2c2ba2d9135a20a6f
Pheb63 <89520387+Pheb63@users.noreply.github.com>
2022-09-22 10:16:05 +0300

    Actual progress

--- Design factory project - Calculator/multipliers.py
+++ Design factory project - Calculator/multipliers.py
@@ -9,7 +9,7 @@ AverageElectricity = 281.00
 #kgCO2e/hkm
 shortFlightUnder463km = 0.2595*2+0.2595*2
 longFligtsOverHomelandOver463km = (0.178*2)+(0.178*0.2)
-longFlightsAbroadOver463km = (0,149*2)+(0.149*0.2)
+longFlightsAbroadOver463km = (0.149*2)+(0.149*0.2)
 longFlightsOver3700km = (0.135*2)+(0.135*0.2)
 
     
