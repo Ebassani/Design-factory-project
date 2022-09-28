57a2671a04ae830b6e97a6af4df717e268df3575
Pheb63 <89520387+Pheb63@users.noreply.github.com>
2022-09-28 15:18:13 +0300

    Update food.py

--- Design factory project - Calculator/food.py
+++ Design factory project - Calculator/food.py
@@ -119,7 +119,7 @@ def surplusFood():
     sides = amountKG #=((=VLOOKUP(E8,Kertoimet!B145:C148,2,FALSE)/1000*150)/150+(=VLOOKUP(E14,Kertoimet!B145:C148,2,FALSE)/1000*150)/150+(=VLOOKUP(E20,Kertoimet!B145:C148,2,FALSE)/1000*150)/150)/3*amountKG*1000
     salad = multipliers.aPortionOfSalad / 150 * 1000 * amountKG
     bread = (multipliers.ryeBread + multipliers.wheatBread) / 2 * amountKG
-    dessert = ((multipliers.PortionOfKisel + multipliers.aPortionOfPancakes)/2)/150*1000*amountKG
+    dessert = ((multipliers.aPortionOfKisel + multipliers.aPortionOfPancakes)/2)/150*1000*amountKG
     return meat + veggie + vegan + sides + salad + bread + dessert
 
 # IN PROGRESS
